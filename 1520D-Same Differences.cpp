fd45877a9ac14c105d5ca6446b68e58402f669dd
bhalerao-2002 <96150629+bhalerao-2002@users.noreply.github.com>
2022-07-23 04:14:58 +0530

    1520D-Same Differences-CodeForces-Bhalerao-2002

---
+++ 1520D-Same Differences.cpp
@@ -0,0 +1,24 @@
+//Thanks TO Bhalerao-2002
+#include <iostream>
+#include <map>
+#define ll long long int
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        int x;
+        map<int,ll> mp;
+        ll maxi = 0;
+        for (int i = 0; i < n; ++i) {
+            cin>>x;
+            maxi += mp[x-i];
+            mp[x-i]++;
+        }
+        cout<<maxi<<"\n";
+    }
+}
